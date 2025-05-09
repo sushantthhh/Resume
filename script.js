73c7e415d2f47264fa7393ff9ef3e7ab070eb1c2
sushantthhh <103987949+sushantthhh@users.noreply.github.com>
2025-05-10 04:36:25 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSkills() {
+  const skills = document.getElementById("skills");
+  if (skills.style.display === "none") {
+    skills.style.display = "block";
+  } else {
+    skills.style.display = "none";
+  }
+}
