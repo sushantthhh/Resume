fd9e4264d6dea01c2f60336e8cb66a92f53788ca
sushantthhh <103987949+sushantthhh@users.noreply.github.com>
2025-05-10 12:52:33 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const buttons = document.querySelectorAll('.info-btn');
 const bubbles = document.querySelectorAll('.bubble');
+const container = document.querySelector('.resume-container');
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const targetId = button.getAttribute('data-target');
     const target = document.getElementById(targetId);
 
-    // If already visible, toggle it off
     const isActive = target.classList.contains('show');
     bubbles.forEach(b => b.classList.remove('show'));
+
     if (!isActive) {
       target.classList.add('show');
     }
+
+    container.classList.add('shrink');
   });
 });
